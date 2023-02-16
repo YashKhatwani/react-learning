db989b173529181c86e389040b56d2714e13e892
YASH <yashkhatwani2002@gmail.com>
2023-02-17 04:30:53 +0530

    Home page and metamask login

---
+++ src/components/Video.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Video() {
+    let topic = "React Js";
+    return (
+        <div>
+            <img src="http://placeimg.com/640/360/any" alt="" />
+            <div style={{ backgroundColor: "red" }}>{topic} Tutorial by Abhishek Rathod</div>
+        </div>
+    )
+}
+
+export default Video
